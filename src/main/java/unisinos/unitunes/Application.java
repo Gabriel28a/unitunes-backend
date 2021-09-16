c43ef731a812988f9e874bf2e3c06b8310f58a70
Gabriel Bittencourt <gabriel28@edu.unisinos.br>
2021-09-15 21:17:58 -0300

    Test

--- src/main/java/unisinos/unitunes/Application.java
+++ src/main/java/unisinos/unitunes/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UnitunesApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(UnitunesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
